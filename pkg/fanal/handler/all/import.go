6fd0e63f1e437e93fbf4dbc65e8d3b316899dd6b
Sylvain Baubeau <lebauce@gmail.com>
2022-12-21 23:44:34 +0100

    Do not import unused analyzers

--- pkg/fanal/handler/all/import.go
+++ pkg/fanal/handler/all/import.go
@@ -2,7 +2,7 @@ package all
 
 import (
 	_ "github.com/aquasecurity/trivy/pkg/fanal/handler/dpkg"
-	_ "github.com/aquasecurity/trivy/pkg/fanal/handler/gomod"
-	_ "github.com/aquasecurity/trivy/pkg/fanal/handler/misconf"
-	_ "github.com/aquasecurity/trivy/pkg/fanal/handler/sysfile"
+	// _ "github.com/aquasecurity/trivy/pkg/fanal/handler/gomod"
+	// _ "github.com/aquasecurity/trivy/pkg/fanal/handler/misconf"
+	// _ "github.com/aquasecurity/trivy/pkg/fanal/handler/sysfile"
 )
